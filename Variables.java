a35d3291d3f636bd1cd84079e1e62cd58d69d6b9
Arman Darmesh <darmesharman@gmail.com>
2022-09-14 12:21:41 +0600

    Variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte = 100;
+        short myShort = 1234;
+        int myInt = 1234121241;
+        long myLong = 1234123412124124L;
+
+        float myFloat = 123.12f;
+        double myDouble = 123.12;
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+    }
+}
