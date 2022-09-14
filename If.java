b44ae525c0c2b5eee704574e3e9a3afe21a04042
Arman Darmesh <darmesharman@gmail.com>
2022-09-14 13:12:32 +0600

    Conditions

---
+++ If.java
@@ -0,0 +1,13 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+
+        if (myInt > 5) {
+            System.out.println("more");
+        } else if (myInt < 5) {
+            System.out.println("less");
+        } else {
+            System.out.println("equal");
+        }
+    }
+}
