75e7c90c8e8aa92bd66c07eb2f812302c70ac2e1
Arman Darmesh <darmesharman@gmail.com>
2022-09-15 11:52:34 +0600

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,12 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.showName();
+    }
+}
