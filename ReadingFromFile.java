d9edc764ebe32a7e3ded1c2bc387ef211fc35de6
Arman Darmesh <darmesharman@gmail.com>
2022-09-19 16:43:55 +0600

    Reading from File

---
+++ ReadingFromFile.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadingFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input");
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+    }
+}
